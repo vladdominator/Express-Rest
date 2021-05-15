128e2de9cc48276c14f43dcb36668a073e7a565b
vladdominator <https://github.com/vladdominator>
2021-05-15 16:03:26 +0300

    feat: add users router

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,43 @@
-const getAll = async () => 
-  // TODO: mock implementation. should be replaced during task development
-   []
-;
+const uuid = require('uuid');
+const database = require('../../dbParametres/db');
 
-module.exports = { getAll };
+const {users} = database.db;
+const getAll = async () => users;
+const getUser = async (id) => {
+   if (typeof id !== 'string') return null;
+   return users.find(user => user.id === id);
+};
+const setUser = async (user) => {
+   const newUser = { ...user, id: uuid.v4() };
+   users.push(newUser);
+   return newUser;
+};
+const updateUser = async (id, userData) => {
+  let index;
+  users.forEach((element, i) => {
+     if(element.id === id) {
+        index = i;
+     }
+  });
+  if (index >= 0) {
+    const newUser = { ...userData, id };
+    users[index] = newUser;
+    return newUser;
+  }
+  return index;
+};
+const deleteUser = async (id) => {
+   let index;
+   users.forEach((element, i) => {
+      if(element.id === id) {
+         index = i;
+      }
+   });
+   if (index >= 0) {
+     users.splice(1, index);
+   }
+   return index;
+};
+
+
+module.exports = { getAll, getUser, setUser, updateUser, deleteUser };
